bfd95bfc121a6cabe3c1a93ba8ab46eb7bc9d108
Ashley Richard <ashleymrichard@gmail.com>
2022-04-16 08:21:52 -0700

    Displays a bunch of Pokémon images on the index.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const Pokemon = require('./models/pokemon.js');
+
+// INDEX
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', { 
+        data: Pokemon
+    });
+});
+
+// SHOW
+app.get('/:id', (req, res) => {
+res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+app.listen(3000, () => {
+    console.log(`Listening on port: ${PORT}`)
+});
