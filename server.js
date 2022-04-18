a2015767fa4f3a08b246c159afd60c86d8a14af5
Ashley Richard <ashleymrichard@gmail.com>
2022-04-18 10:53:06 -0700

    Has the ability to add a new Pokémon

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+let bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 3001;
 const Pokemon = require('./models/pokemon.js');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 // INDEX
 app.get('/pokemon', (req, res) => {
@@ -10,11 +14,33 @@ app.get('/pokemon', (req, res) => {
     });
 });
 
+//NEW
+app.get('/pokemon/new', (req,res) => {
+    res.render('new.ejs');
+});
+
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', { 
         data: Pokemon[req.params.id]
     });
+});
+
+//Create New
+app.post('/pokemon', (req,res) => {
+    const newPokemon = 
+    {
+        name: req.body.name,
+        type: req.body.type.split(','),
+        img: req.body.img,
+        stats: {
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense
+        }
+    }
+    Pokemon.push(newPokemon);
+    res.redirect('/pokemon');
 });
 
 app.listen(3000, () => {
